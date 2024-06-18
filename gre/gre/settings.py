ebafd6eaaacda919e3a62e272657b814e9e8e476
YM <109154005@g.nccu.edu.com>
2024-06-18 20:47:59 +0800

    6/18

--- gre/gre/settings.py
+++ gre/gre/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'members'
+    'members',
+    'bootstrap5',
+
 ]
 
 MIDDLEWARE = [
